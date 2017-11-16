ddeabf535536ea8ccc16a12d708a8f4f24b33aed
Taha Abbasi <tahaabbasi@me.com>
2017-11-16 12:05:40 -0500

    Static folder setup

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello Express!');
-});
+var PORT = 3000;
 
 app.get('/about', function (req, res) {
     res.send('About Us');
 });
 
-app.listen(3000);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+    console.log('Express server started on port ' + PORT + '!');
+});
